dad3070f304ab2a3ba1735e0af7d3bcd4613f8f5
FarjanaFerdousiTamanna <farjana.ferdousi.tamanna@gmail.com>
2020-08-18 12:40:22 +0600

    Add project files.

---
+++ FourthProject/People/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthProject.People  //thid is the new class we created inside a namespace 
+                                // here we have to mention the namespace name so that it can relate. 
+{
+    class Person
+    {
+        // mentioning 3 fields
+        public string name;
+        public int age;
+        public bool haspets;
+
+        // creating a method
+        public void greetings() 
+        {
+            Console.WriteLine("My name is " + name + " and my age is " + age + ".");
+        
+        }
+    }
+}
---
+++ FourthProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FourthProject.People; // as this is the main class, here we have to add 
+                            // every new namespace we creat. we created a namespace named
+                            //People so we have written that like "using fourthProject.People"
+
+namespace FourthProject
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person= new Person(); // saving it in the memory // class name 
+            person.name = "Tamanna";
+            person.age = 23;
+            person.haspets = true;
+
+            person.greetings();
+        }
+    }
+}
